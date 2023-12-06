3be840a703b83cdec3cdf2915261a7bfef4fb994
shahzaibali2122 <shahzaibkarim74@gmail.com>
2023-12-06 19:45:27 +0500

    button dimissions

--- App.tsx
+++ App.tsx
@@ -197,10 +197,10 @@ const App = () => {
 
     <View>
       <Charts />
-      <View style={{marginTop: 50}}>
+      <View style={{marginTop: 24}}>
         <LineCharts />
       </View>
-      <View style={{marginTop: 50}}>
+      <View style={{marginTop: 24}}>
       <BarCharts />
       </View>
     </View>
